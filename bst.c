17d51bba40242692cb4772cc9b6e8af5216c5f91
David Kleber <davidkmsantos@gmail.com>
2024-01-29 13:14:22 -0300

    primeiro commit

---
+++ bst.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no
+{
+    int valor;
+    struct no *esq;
+    struct no *dir;
+} no;
+typedef no *arvore;
+
+int main()
+{
+    return 0;
+}
