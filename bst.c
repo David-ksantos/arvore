9e635d5fe5b0725fbd9f820da4bb5e39b264f55f
David Kleber <davidkmsantos@gmail.com>
2024-01-29 13:25:12 -0300

    com 2

--- bst.c
+++ bst.c
@@ -9,6 +9,31 @@ typedef struct no
 } no;
 typedef no *arvore;
 
+arvore inserir(arvore raiz, int valor)
+{
+    if (raiz == NULL)
+    {
+        arvore novo = (arvore)malloc(sizeof(no));
+        novo->valor = valor;
+        novo->esq = NULL;
+        novo->dir = NULL;
+        return novo;
+    }
+
+    else
+    {
+        if (valor > raiz->valor)
+        {
+            raiz->dir = inserir(raiz->dir, valor);
+        }
+        else
+        {
+            raiz->esq = inserir(raiz->esq, valor);
+        }
+        return raiz;
+    }
+}
+
 int main()
 {
     return 0;
